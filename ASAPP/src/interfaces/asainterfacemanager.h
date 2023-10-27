f4f64bea943a6f53a0ad5c7505265cc842226bcf
Kenny <106347478+kennyhml@users.noreply.github.com>
2023-10-27 23:53:37 +0200

    added interfacemanager

---
+++ ASAPP/src/interfaces/asainterfacemanager.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "asainterface.h"
+
+namespace asa
+{
+	// needs all tabs: inventory, engrams, tribe, tracking, notes & map
+	// also player stats  & dino stat tab, personal inventory & other inventory
+	class ASAInterfaceManager : ASAInterface
+	{
+	};
+
+}
---
+++ ASAPP/src/interfaces/asainventory.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "asainterface.h"
+
+namespace asa
+{
+	class ASAInventory : ASAInterface
+	{
+	};
+
+}
