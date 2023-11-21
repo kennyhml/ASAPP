d11d81ae3fa9a23c935f22ed73f50f3c5b92c564
Kenny <106347478+kennyhml@users.noreply.github.com>
2023-11-21 20:09:57 +0100

    implemented a baseinfo

--- ASAPP/src/interfaces/baseentityinfo.h
+++ ASAPP/src/interfaces/baseentityinfo.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "../game/window.h"
+#include "iinterface.h"
+
+namespace asa::interfaces
+{
+
+	class BaseEnityInfo : public IInterface
+	{
+
+	private:
+		window::Rect area{ 747, 109, 429, 806 };
+	};
+
+
+
+
+
+
+}
---
+++ ASAPP/src/interfaces/baseinfo.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "../game/window.h"
+#include "iinterface.h"
+
+namespace asa::interfaces
+{
+	class BaseInfo : public IInterface
+	{
+	protected:
+		window::Rect area{ 747, 109, 429, 806 };
+
+	public:
+		virtual const int GetMaxHealth() = 0;
+		virtual const int GetCurrentHealth() = 0;
+
+		virtual const int GetMaxWeight() = 0;
+		virtual const int GetCurrentWeight() = 0;
+
+		virtual const std::string GetOwner() = 0;
+		virtual const std::string GetName() = 0;
+	};
+
+}
