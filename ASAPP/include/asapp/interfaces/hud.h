639b17a3e17def162318409bce7f3edfc28412bc
Kenny <106347478+kennyhml@users.noreply.github.com>
2023-12-24 13:55:07 +0100

    adjusted hud

--- ASAPP/include/asapp/interfaces/hud.h
+++ ASAPP/include/asapp/interfaces/hud.h
@@ -16,41 +16,56 @@ namespace asa::interfaces
         [[nodiscard]] bool is_mount_out_of_stamina() { return false; };
         [[nodiscard]] bool is_mount_sprinting() { return false; };
 
-        [[nodiscard]] bool is_player_overweight();
-        [[nodiscard]] bool is_player_broken_bones();
-        [[nodiscard]] bool is_player_out_of_water();
-        [[nodiscard]] bool is_player_out_of_food();
-        [[nodiscard]] bool is_player_sprinting();
+        [[nodiscard]] bool is_player_overweight() const;
+        [[nodiscard]] bool is_player_broken_bones() const;
+        [[nodiscard]] bool is_player_out_of_water() const;
+        [[nodiscard]] bool is_player_out_of_food() const;
+        [[nodiscard]] bool is_player_sprinting() const;
 
-        [[nodiscard]] bool can_default_teleport();
-        [[nodiscard]] bool can_fast_travel();
-        [[nodiscard]] bool can_access_inventory();
+        [[nodiscard]] bool can_default_teleport() const;
+        [[nodiscard]] bool can_fast_travel() const;
+        [[nodiscard]] bool can_access_inventory() const;
 
-        [[nodiscard]] bool extended_information_is_toggled();
-        [[nodiscard]] bool item_added(items::Item*, window::Rect* roi_out);
-        [[nodiscard]] bool item_removed(items::Item*, window::Rect* roi_out);
-        [[nodiscard]] bool transferred_item_into_dedicated_storage();
+        [[nodiscard]] bool extended_information_is_toggled() const;
 
-        bool count_items_added(items::Item&, int& amountOut);
-        bool count_items_removed(items::Item&, int& amountOut);
+        /**
+         * @brief Checks whether an item was removed in the given area
+         * 
+         * @param area The area to check whether an item was removed.
+         * 
+         * @return True if an item was removed, false otherwise.  
+         */
+        [[nodiscard]] static bool item_removed(const window::Rect& area) const;
+
+        /**
+         * @brief Checks whether an item was added in the given area
+         * 
+         * @param area The area to check whether an item was added.
+         * 
+         * @return True if an item was added, false otherwise.  
+         */
+        [[nodiscard]] static bool item_added(const window::Rect& area) const;
+        [[nodiscard]] bool item_added(items::Item&, window::Rect* roi_out) const;
+        [[nodiscard]] bool item_removed(items::Item&, window::Rect* roi_out) const;
+        [[nodiscard]] bool transferred_item_into_dedicated_storage() const;
+
+        bool count_items_added(items::Item&, int& amount_out) const;
+        bool count_items_removed(items::Item&, int& amount_out) const;
 
         float get_water_amount() const;
         float get_food_amount() const;
         float get_weight_amount() const;
 
     private:
-        window::Color blink_red_state{109, 54, 52};
-        window::Color blink_red_state_weight{255, 45, 45};
+        window::Color blink_red_state_{109, 54, 52};
+        window::Color blink_red_state_weight_{255, 45, 45};
 
-        window::Rect water_icon{1868, 806, 34, 46};
-        window::Rect food_icon{1861, 858, 46, 41};
-        window::Rect stamina_icon{1872, 900, 29, 51};
-        window::Rect health_icon{1861, 953, 48, 45};
-        window::Rect weight_icon{1860, 751, 51, 56};
-        window::Rect default_teleport{866, 911, 78, 27};
-
-        [[nodiscard]] bool item_removed(const window::Rect& area);
-        [[nodiscard]] bool item_added(const window::Rect& area);
+        window::Rect water_icon_{1868, 806, 34, 46};
+        window::Rect food_icon_{1861, 858, 46, 41};
+        window::Rect stamina_icon_{1872, 900, 29, 51};
+        window::Rect health_icon_{1861, 953, 48, 45};
+        window::Rect weight_icon_{1860, 751, 51, 56};
+        window::Rect default_teleport_{866, 911, 78, 27};
 
         window::Rect item_icon_removed_or_added_area{7, 12, 41, 1067};
         window::Rect item_removed_area{43, 20, 110, 1054};
--- ASAPP/source/interfaces/hud.cpp
+++ ASAPP/source/interfaces/hud.cpp
@@ -1,6 +1,7 @@
 #include "asapp/interfaces/hud.h"
 #include "asapp/util/util.h"
 #include <iostream>
+#include <opencv2/highgui.hpp>
 #include "asapp/game/resources.h"
 #include "asapp/game/window.h"
 
@@ -8,130 +9,118 @@ namespace asa::interfaces
 {
     namespace
     {
-        bool is_blinking(window::Rect icon, window::Color color,
-                         std::chrono::milliseconds maxDuration =
+        bool is_blinking(const window::Rect& icon, const window::Color& color,
+                         const std::chrono::milliseconds timeout =
                              std::chrono::milliseconds(500))
         {
-            auto start = std::chrono::system_clock::now();
-            while (!util::timedout(start, maxDuration)) {
-                auto masked = window::get_mask(icon, color, 30);
-                if (cv::countNonZero(masked) > 20) { return true; }
+            const auto start = std::chrono::system_clock::now();
+            while (!util::timedout(start, timeout)) {
+                if (cv::countNonZero(window::get_mask(icon, color, 30)) > 20) {
+                    return true;
+                }
             }
             return false;
         }
     }
 
-    bool HUD::item_removed(const window::Rect& area)
+    bool HUD::is_player_overweight() const
     {
-        return window::match_template(area, resources::text::removed);
-    }
-
-    bool HUD::item_added(const window::Rect& area)
-    {
-        return window::match_template(area, resources::text::added);
-    }
-
-    bool HUD::is_player_overweight()
-    {
-        return is_blinking(weight_icon, blink_red_state_weight,
+        return is_blinking(weight_icon_, blink_red_state_weight_,
                            std::chrono::milliseconds(700));
     }
 
-    bool HUD::is_player_broken_bones()
+    bool HUD::is_player_broken_bones() const
     {
-        return is_blinking(health_icon, blink_red_state);
+        return is_blinking(health_icon_, blink_red_state_);
     }
 
-    bool HUD::is_player_out_of_water()
+    bool HUD::is_player_out_of_water() const
     {
-        return is_blinking(water_icon, blink_red_state);
+        return is_blinking(water_icon_, blink_red_state_);
     }
 
-    bool HUD::is_player_out_of_food() { return is_blinking(food_icon, blink_red_state); }
-
-    bool HUD::is_player_sprinting() { return false; }
-
-    bool HUD::can_default_teleport()
+    bool HUD::is_player_out_of_food() const
     {
-        return window::match_template(default_teleport, resources::text::default_teleport,
-                                      0.5);
+        return is_blinking(food_icon_, blink_red_state_);
     }
 
-    bool HUD::can_fast_travel()
+    bool HUD::is_player_sprinting() const { return false; }
+
+    bool HUD::can_default_teleport() const
+    {
+        return window::match_template(default_teleport_,
+                                      resources::text::default_teleport, 0.5);
+    }
+
+    bool HUD::can_fast_travel() const
     {
         return window::match_template(window::screenshot(), resources::text::fast_travel);
     }
 
-    bool HUD::can_access_inventory()
+    bool HUD::can_access_inventory() const
     {
         return window::match_template(window::screenshot(),
                                       resources::text::access_inventory);
     }
 
-    bool HUD::extended_information_is_toggled()
+    bool HUD::extended_information_is_toggled() const
     {
         static window::Rect roi{14, 34, 134, 35};
         return window::match_template(roi, resources::text::day);
     }
 
-    bool HUD::item_added(items::Item* item, window::Rect* roi_out)
+    bool HUD::item_added(items::Item& item, window::Rect* roi_out) const
     {
-        auto roi = item ? item_icon_removed_or_added_area : item_added_area;
-        if (!item) { return item_added(roi); }
-        auto locations = window::locate_all_template(roi, item->get_notification_icon(),
-                                                     0.75, item->
-                                                     get_notification_icon_mask());
+        const window::Rect roi = item_icon_removed_or_added_area;
+        const std::vector<window::Rect> locations = window::locate_all_template(
+            roi, item.get_notification_icon(), 0.75f, item.get_notification_icon_mask());
 
-        for (const auto& rect : locations) {
-            window::Rect match_roi(roi.x + rect.x + 20, roi.y + rect.y, 120, 25);
-            if (item_added(match_roi)) {
-                if (roi_out) { *roi_out = match_roi; }
-                return true;
-            }
-        }
-        return false;
+        return std::ranges::any_of(locations.begin(), locations.end(),
+                                   [roi](const window::Rect& rect) -> bool {
+                                       return item_added({
+                                           roi.x + rect.x + 20, roi.y + rect.y - 10, 120,
+                                           25
+                                       });
+                                   });
     }
 
-    bool HUD::item_removed(items::Item* item, window::Rect* roi_out)
+    bool HUD::item_removed(items::Item& item, window::Rect* roi_out) const
     {
-        auto roi = item ? item_icon_removed_or_added_area : item_removed_area;
-        if (!item) { return item_removed(roi); }
-        auto locations = window::locate_all_template(roi, item->get_notification_icon(),
-                                                     0.75, item->
-                                                     get_notification_icon_mask());
+        const window::Rect roi = item_icon_removed_or_added_area;
+        const std::vector<window::Rect> locations = window::locate_all_template(
+            roi, item.get_notification_icon(), 0.75f, item.get_notification_icon_mask());
 
-        for (const auto& rect : locations) {
-            window::Rect match_roi(roi.x + rect.x + 20, roi.y + rect.y, 120, 25);
-            if (item_removed(match_roi)) {
-                if (roi_out) { *roi_out = match_roi; }
-                return true;
-            }
-        }
-        return false;
+        return std::ranges::any_of(locations.begin(), locations.end(),
+                                   [roi](const window::Rect& rect) -> bool {
+                                       return item_removed({
+                                           roi.x + rect.x + 20, roi.y + rect.y - 10, 120,
+                                           30
+                                       });
+                                   });
     }
 
-    bool HUD::count_items_added(items::Item& item, int& amount_out)
+    bool HUD::count_items_added(items::Item& item, int& amount_out) const
     {
         window::Rect roi{0, 0, 0, 0};
-        if (!item_added(&item, &roi)) { return false; }
+        if (!item_added(item, &roi)) { return false; }
 
         roi = {roi.x + 85, roi.y, 100, roi.height};
 
         auto x_loc = window::locate_template(roi, resources::text::x);
         if (!x_loc.has_value()) {
-            std::cerr << "[!] Failed to locate x location." << std::endl;
+            std::cerr << "[!] Failed to locate x location.\n";
             return false;
         }
 
         roi = {roi.x, roi.y, x_loc->x, roi.height};
-        cv::Mat mask = window::get_mask(roi, window::Color(255, 255, 255), 50);
+        const cv::Mat mask = window::get_mask(roi, window::Color{255, 255, 255}, 50);
         window::set_tesseract_image(mask);
         window::tessEngine->SetPageSegMode(tesseract::PSM_SINGLE_WORD);
         window::tessEngine->SetVariable("tessedit_char_whitelist", "0123456789");
 
-        std::string result_string = window::tessEngine->GetUTF8Text();
+        const std::string result_string = window::tessEngine->GetUTF8Text();
         if (result_string.empty() || result_string == "\\n") {
-            std::cerr << "[!] OCR failed, no result determined." << std::endl;
+            std::cerr << "[!] OCR failed, no result determined.\n";
             return false;
         }
 
@@ -139,28 +128,28 @@ namespace asa::interfaces
         return true;
     }
 
-    bool HUD::count_items_removed(items::Item& item, int& amount_out)
+    bool HUD::count_items_removed(items::Item& item, int& amount_out) const
     {
         window::Rect roi{0, 0, 0, 0};
-        if (!item_removed(&item, &roi)) { return false; }
+        if (!item_removed(item, &roi)) { return false; }
 
         roi = {roi.x + 110, roi.y, 100, roi.height};
 
         auto x_loc = window::locate_template(roi, resources::text::x);
         if (!x_loc.has_value()) {
-            std::cerr << "[!] Failed to locate x location." << std::endl;
+            std::cerr << "[!] Failed to locate x location.\n";
             return false;
         }
 
         roi = {roi.x, roi.y, x_loc->x, roi.height};
-        cv::Mat mask = window::get_mask(roi, window::Color(255, 255, 255), 50);
+        cv::Mat mask = window::get_mask(roi, window::Color{255, 255, 255}, 50);
         window::set_tesseract_image(mask);
         window::tessEngine->SetPageSegMode(tesseract::PSM_SINGLE_WORD);
         window::tessEngine->SetVariable("tessedit_char_whitelist", "0123456789");
 
-        std::string result_string = window::tessEngine->GetUTF8Text();
+        const std::string result_string = window::tessEngine->GetUTF8Text();
         if (result_string.empty() || result_string == "\\n") {
-            std::cerr << "[!] OCR failed, no result determined." << std::endl;
+            std::cerr << "[!] OCR failed, no result determined.\n";
             return false;
         }
 
@@ -171,15 +160,23 @@ namespace asa::interfaces
     float HUD::get_water_amount() const
     {
         static window::Rect roi(1885, 806, 1, 43);
-        static window::Color full(111, 150, 155);
+        static window::Color full{111, 150, 155};
 
-        cv::Mat masked = window::get_mask(roi, full, 30);
-
-        int amount_filled = cv::countNonZero(masked);
+        const int amount_filled = cv::countNonZero(window::get_mask(roi, full, 30));
         return (static_cast<float>(amount_filled) / static_cast<float>(roi.height));
     }
 
     float HUD::get_food_amount() const { return 0; }
 
     float HUD::get_weight_amount() const { return 0; }
+
+    bool HUD::item_removed(const window::Rect& area) const
+    {
+        return window::match_template(area, resources::text::removed);
+    }
+
+    bool HUD::item_added(const window::Rect& area) const
+    {
+        return window::match_template(area, resources::text::added);
+    }
 }
