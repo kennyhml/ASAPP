4b3d6f19dfc7796596d4ecf2aec9eceea7f5d2fd
Kenny <106347478+kennyhml@users.noreply.github.com>
2024-01-12 22:58:16 +0100

    added custom info for dedi

--- ASAPP/include/asapp/structures/dedicatedstorage.h
+++ ASAPP/include/asapp/structures/dedicatedstorage.h
@@ -1,13 +1,16 @@
 #pragma once
 #include "container.h"
 #include "../interfaces/baseinventory.h"
+#include "../interfaces/dedicatedstorageinfo.h"
 
 namespace asa::structures
 {
     class DedicatedStorage final : public Container
     {
     public:
-        DedicatedStorage() : Container("Dedicated Storage", 1800) {}
+        explicit DedicatedStorage(std::string name) : Container(
+            std::move(name), 1800, nullptr,
+            std::make_unique<interfaces::DedicatedStorageInfo>()) {}
 
         /**
          * @brief Checks whether depositing into the dedicated storage is possible.
@@ -15,5 +18,17 @@ namespace asa::structures
          * @return True if we can deposit items into the dedi, false otherwise.
          */
         [[nodiscard]] bool can_deposit() const;
+
+        /**
+         * @brief Gets the info interface component of the dedicated storage. 
+         *
+         * @remark This is a special component that allows item withdrawal.
+         */
+        [[nodiscard]] interfaces::DedicatedStorageInfo* get_info() const override
+        {
+            return dynamic_cast<interfaces::DedicatedStorageInfo*>(info_.get());
+        }
+
+    private:
     };
 }
