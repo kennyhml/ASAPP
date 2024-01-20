740fb987d7ad3b8cb2952bd8b896bfc43cc35e55
Kenny <106347478+kennyhml@users.noreply.github.com>
2024-01-20 21:19:19 +0100

    implemented structure health info

--- ASAPP/include/asapp/interfaces/containerinfo.h
+++ ASAPP/include/asapp/interfaces/containerinfo.h
@@ -6,7 +6,8 @@ namespace asa::interfaces
     class ContainerInfo : public BaseInfo
     {
     private:
-        window::Rect slots_bar{768, 548, 382, 6};
+        window::Rect health_bar_{768, 512, 382, 6};
+        window::Rect slots_bar_{768, 548, 382, 6};
 
     public:
         enum Stat
@@ -18,12 +19,12 @@ namespace asa::interfaces
 
         bool is_open() const override { return true; }
 
-        int get_max_health() override { return 0; };
-        int get_current_health() override { return 0; };
-        float get_health_level() override { return 0; };
+        int get_max_health() override { return 0; }
+        int get_current_health() override { return 0; }
+        float get_health_level() override;
 
         virtual int get_max_slots() { return 0; };
-        virtual int get_current_slots() { return 0; };
+        virtual int get_current_slots() { return 0; }
         virtual float get_fill_level();
 
         int get_max_weight() override;
--- ASAPP/source/interfaces/structureinfo.cpp
+++ ASAPP/source/interfaces/structureinfo.cpp
@@ -2,15 +2,31 @@
 
 namespace asa::interfaces
 {
+    namespace
+    {
+        float get_filled(const window::Rect& bar)
+        {
+            static constexpr window::Color fill_color{1, 156, 136};
+            const auto mask = window::get_mask(bar, fill_color, 10);
+            const int green = cv::countNonZero(mask);
+
+            return static_cast<float>(green) / static_cast<float>(bar.width);
+        }
+    }
+
     float ContainerInfo::get_fill_level()
     {
-        auto roi = slots_bar;
+        auto roi = slots_bar_;
+        roi.y -= 5;
+        return get_filled(roi);
+    }
+
+    float ContainerInfo::get_health_level()
+    {
+        auto roi = health_bar_;
         roi.y -= 5;
 
-        auto masked = window::get_mask(roi, window::Color(1, 156, 136), 10);
-        int greenPixels = cv::countNonZero(masked);
-
-        return static_cast<float>(greenPixels) / static_cast<float>(slots_bar.width);
+        return get_filled(roi);
     }
 
     int ContainerInfo::get_max_weight() { return std::numeric_limits<float>::infinity(); }
