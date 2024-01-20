120b1ba860ecc9744d629595aad37ab3692048f2
Kenny <106347478+kennyhml@users.noreply.github.com>
2024-01-20 11:45:43 +0100

    added Console

---
+++ ASAPP/include/asapp/interfaces/console.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "iinterface.h"
+#include <string>
+
+#include "asapp/game/window.h"
+
+namespace asa::interfaces
+{
+    class Console final : public IInterface
+    {
+    public:
+        Console() = default;
+
+        /**
+         * @brief Checks whether the console is currently open. 
+         * \return 
+         */
+        [[nodiscard]] bool is_open() const override;
+
+        /**
+         * @brief Opens the console. 
+         */
+        void open();
+
+        /**
+         * @brief Closes the console. 
+         */
+        void close();
+
+        /**
+         * @brief Executes a command in the console.
+         * 
+         * @param command The command to execute.
+         */
+        void execute(const std::string& command);
+
+    private:
+        window::Rect bar_lower_{0, 1059, 1919, 20};
+    };
+}
---
+++ ASAPP/source/interfaces/console.cpp
@@ -0,0 +1,46 @@
+#include "asapp/interfaces/console.h"
+
+#include <iostream>
+#include <asapp/util/util.h>
+
+
+namespace asa::interfaces
+{
+    bool Console::is_open() const
+    {
+        static constexpr window::Color gray{140,140,140};
+
+        return cv::countNonZero(window::get_mask(bar_lower_, gray, 5)) > 500; 
+
+        
+    }
+
+    void Console::open()
+    {
+        if (is_open()) { return; }
+
+        do { window::press("`"); }
+        while (!util::await([this]() -> bool { return is_open(); },
+                            std::chrono::seconds(5)));
+    }
+
+    void Console::close()
+    {
+        if (!is_open()) { return; }
+
+        do { window::press("enter"); }
+        while (!util::await([this]() -> bool { return !is_open(); },
+                            std::chrono::seconds(5)));
+    }
+
+    void Console::execute(const std::string& command)
+    {
+        open();
+        util::set_clipboard(command);
+        controls::key_combination_press("ctrl", "v");
+        window::press("enter");
+    }
+
+
+
+}
