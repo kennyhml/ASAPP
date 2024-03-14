d43d8345dd418ca227a19fcf21137692174c86de
Kenny <106347478+kennyhml@users.noreply.github.com>
2024-03-14 21:37:44 +0100

    improved tooltip

--- ASAPP/include/asapp/interfaces/components/tooltip.h
+++ ASAPP/include/asapp/interfaces/components/tooltip.h
@@ -1,13 +1,39 @@
 #pragma once
 #include "iinterfacecomponent.h"
 
-// 121,244,253
-
 namespace asa::interfaces::components
 {
     class ItemTooltip : public IInterfaceComponent
     {
     public:
         using IInterfaceComponent::IInterfaceComponent;
+
+        /**
+         * @brief Constructs a new item tooltip from an area while hovered meaning the
+         * image for the tooltip is automatically snapped during construction.
+         *
+         * @warning This constructor should only be used when the resulting tooltip
+         * is currently hovered.
+         *
+         * @param area The area of the tooltip to be created (thats hovered)
+         * @return
+         */
+        static ItemTooltip from_hovered(const window::Rect& area);
+
+        /**
+         * @return Gets the image of the tooltip, an image must have been taken before.
+         */
+        cv::Mat get_image() { return image_; }
+
+        /**
+         * @return Takes the image of the tooltip, it must be visible at this point.
+         */
+        void take_image() { image_ = window::screenshot(area); }
+
+        [[nodiscard]] std::optional<window::Rect> get_durability_area() const;
+
+
+    private:
+        cv::Mat image_;
     };
 }
---
+++ ASAPP/source/interfaces/components/tooltip.cpp
@@ -0,0 +1,24 @@
+#include "asapp/interfaces/components/tooltip.h"
+
+#include "asapp/game/resources.h"
+
+
+namespace asa::interfaces::components
+{
+    ItemTooltip ItemTooltip::from_hovered(const window::Rect& area)
+    {
+        ItemTooltip ret(area);
+        ret.take_image();
+        return ret;
+    }
+
+    std::optional<window::Rect> ItemTooltip::get_durability_area() const
+    {
+        if (image_.empty()) { return std::nullopt; }
+
+        const auto res = window::locate_template(image_, resources::text::durability);
+        if (!res.has_value()) { return std::nullopt; }
+
+        return window::Rect(res->x, res->y, res->width * 2, res->height);
+    }
+}
